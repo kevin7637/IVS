18e17378885465c838fb01f65c0eed38d81b7e58
kevin7637 <68199779+kevin7637@users.noreply.github.com>
2024-04-03 10:41:49 +0900

    test LED1
    first test

---
+++ code/test1/LED1.py
@@ -0,0 +1,28 @@
+import RPi.GPIOas GPIO
+import time
+pin_led_1 = 5
+pin_led_2 = 6
+pin_led_3 = 13
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_led_1, GPIO.OUT)
+GPIO.setup(pin_led_2, GPIO.OUT)
+GPIO.setup(pin_led_3, GPIO.OUT)
+def control_led(pin, state):
+    GPIO.output(pin, state)
+    try:
+        while True:
+            control_led(pin_led_1, True)
+            time.sleep(0.1)
+            control_led(pin_led_1, False)
+            time.sleep(0.1)
+            control_led(pin_led_2, True)
+            time.sleep(0.1)
+            control_led(pin_led_2, False)
+            time.sleep(0.1)
+            control_led(pin_led_3, True)
+            time.sleep(0.1)
+            control_led(pin_led_3, False)
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+    GPIO.cleanup()
