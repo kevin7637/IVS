fad69de38981e63adf9de9568dd781317d957964
kevin7637 <kevin7637@naver.com>
2024-04-08 11:46:19 +0900

    Update PID.py

--- code/control/PID.py
+++ code/control/PID.py
@@ -19,15 +19,12 @@ def yaw_controll(error,MAX_ERROR):
     #300이 0도, 250 우회전, 350이 좌회전
     #오차는 lane_width만큼 생김 좌 0.2m, 우 -0.2
     #yaw값은 servo_tick = (-error + 0.3) * (500 - 100) / (0.3 - (-0.3)) + 100
-    RIGHT_MAX = 370
-    LEFT_MAX = 230
+    RIGHT_MAX = 230
+    LEFT_MAX = 370
     #MAX_ERROR = 0.3
-    servo_tick = (error + MAX_ERROR) * (RIGHT_MAX - LEFT_MAX) / (MAX_ERROR - (-MAX_ERROR)) + LEFT_MAX
+    servo_tick = (error + MAX_ERROR) * (LEFT_MAX - RIGHT_MAX) / (MAX_ERROR - (-MAX_ERROR)) + RIGHT_MAX
     
-    if servo_tick > 370:
-        servo_tick = 370
-    if servo_tick  < 230:
-        servo_tick = 230
+    servo_tick = min(max(servo_tick, RIGHT_MAX), LEFT_MAX)
     
     return int(servo_tick)
 
