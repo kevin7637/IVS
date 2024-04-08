41173b7e064f0b6971e82cab81d4991811a42909
kevin7637 <kevin7637@naver.com>
2024-04-08 11:09:16 +0900

    Update camera2.py

--- code/camera/camera2.py
+++ code/camera/camera2.py
@@ -48,8 +48,8 @@ def point_tracking(image):
                 centroid_y = int((avg_y + centroid_y) / 2)
 
             output_image = image.copy()
-            cv2.circle(output_image, (centroid_x, centroid_y), 10, (100, 100, 100), -1)
-            cv2.imshow("Output", output_image)
+            #cv2.circle(output_image, (centroid_x, centroid_y), 10, (100, 100, 100), -1)
+            #cv2.imshow("Output", output_image)
 
             return (centroid_x, centroid_y) , output_image
 
