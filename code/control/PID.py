8f4eaad48d5584fed70758480ea14a8933eb7cc3
kevin7637 <kevin7637@naver.com>
2024-04-17 11:14:19 +0900

    dd

--- code/control/PID.py
+++ code/control/PID.py
@@ -6,26 +6,14 @@ class PD_Controller(object):
         self.error_pre = reference - measure
         self.step_time = step_time
         self.u = 0.0
+        self.servo_tick = 0
             
     def ControllerInput(self,measure):
-        reference = 320
+        reference = 0
         error = reference - measure
         error_d = (error - self.error_pre)/self.step_time
         self.u = self.kp*error + self.kd*error_d
         self.error_pre = error
         
-
-def yaw_controll(error,MAX_ERROR):
-    #300이 0도, 250 우회전, 350이 좌회전
-    #오차는 lane_width만큼 생김 좌 0.2m, 우 -0.2
-    #yaw값은 servo_tick = (-error + 0.3) * (500 - 100) / (0.3 - (-0.3)) + 100
-    RIGHT_MAX = 230
-    LEFT_MAX = 370
-    #MAX_ERROR = 0.3
-    servo_tick = (error + MAX_ERROR) * (LEFT_MAX - RIGHT_MAX) / (MAX_ERROR - (-MAX_ERROR)) + RIGHT_MAX
-    
-    servo_tick = min(max(servo_tick, RIGHT_MAX), LEFT_MAX)
-    
-    return int(servo_tick)
 
     
